45bc6594022a1f6140f1d10423ac472540634cf5
kellytate <kelly.tate.dev@gmail.com>
2022-07-09 17:04:17 -0400

    add update feature - incomplete

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -20,7 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,75 +37,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
-
-// import { Sequelize, Model, DataTypes } from 'sequelize'
-
-// const express = require('express')
-// const Sequelize = require('sequelize')
-// const app = express()
-// const port = 3000
-// app.use(express.json());
-
-// const user = 'newuser'
-// const host = 'localhost'
-// const database = 'plantsdb'
-// const password = 'pa5w0rt'
-// const pg_port = '5432'
-
-// const sequelize = new Sequelize(database, user, password, {
-//   host,
-//   pg_port,
-//   dialect: 'postgres',
-//   logging: false
-// })
-
-// sequelize
-// .authenticate()
-// .then(() => {
-// console.log('Connection has been established successfully.');
-// })
-// .catch(err => {
-// console.error('Unable to connect to the database:', err);
-// });
-
-
-// const User = sequelize.define('user', {
-// firstName: {
-// type: Sequelize.STRING,
-// allowNull: false
-// },
-// lastName: {
-// type: Sequelize.STRING
-// // allowNull true
-// }
-// }, {
-// //options
-// });
-
-// User.sync({ force: true }) // Now the `users` table in the database corresponds to the model definition
-// app.get('/', (req, res) => res.json({ message: 'Hello World' }))
-// app.post('/user', async (req, res) => {
-// try {
-// const newUser = new User(req.body)
-// await newUser.save()
-// res.json({ user: newUser }) // Returns the new user that is created in the database
-// } catch(error) {
-// console.error(error)
-// }
-// })
-// app.get('/user/:userId', async (req, res) => {
-// const userId = req.params.userId
-// try {
-// const user = await User.findAll({
-// where: {
-// id: userId
-// }
-// }
-// )
-// res.json({ user })
-// } catch(error) {
-// console.error(error)
-// }
-// })
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 const { pgconn } = require('../db/config')
 
+
 router.get('/', function(req, res) {
   // Check if 'plants' table exists
+    
     pgconn.query("SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = 'plants')", function(err,results) {
     if (err) {
       console.log(err);
@@ -17,7 +19,7 @@ router.get('/', function(req, res) {
 
     // If 'plants' table exists, display plant records.
     else {
-      pgconn.query('SELECT * FROM plants', function(err,results) {
+      pgconn.query("SELECT * , to_char(datelastwatered, 'MM-DD-YYYY') as datelastwatered, to_char(datetowater, 'MM-DD-YYYY') as datetowater FROM plants", function(err,results) {
         if (err) {
           console.log(err);
           res.render('index', { error: 'Database connection failure! '+err.stack, plants: null, title: 'Plant List' });
@@ -31,5 +33,32 @@ router.get('/', function(req, res) {
     }
   });
 });
+
+
+    /* Update table */
+router.post('/update', function(req,res) {
+  var newName = 'monstera';
+  var id = '2';
+  pgconn.query("UPDATE plants SET scientificname= $1::text WHERE id = $2::integer", [newName, id], function(err,results) {
+    if (err) {
+      console.log(err);
+      res.render('index', { error: 'Update failure! '+err.stack, plants: null, title: 'Plant List' });
+    }
+
+    // redirect to the index page
+    else {
+      res.redirect('/');
+    }
+  });
+});
+
+
+router.post('/update', function(req, res) {
+  const text = 'UPDATE users SET scientificname = '
+})
+
+
+
+
 
 module.exports = router;
