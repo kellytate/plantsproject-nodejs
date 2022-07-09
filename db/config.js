326805a5bde2cb3ae9c0aeb0053527c497dd5bf1
kellytate <kelly.tate.dev@gmail.com>
2022-07-09 09:45:17 -0400

    update config - need to check that this works

--- db/config.js
+++ db/config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://postgres:postgres@localhost:5432/plants';
+const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://postgres:postgres@localhost:5432/plantsdb';
 const { Pool } = require('pg');
 
 const pgconn = new Pool({
--- routes/index.js
+++ routes/index.js
@@ -2,28 +2,20 @@ var express = require('express');
 var router = express.Router();
 const { pgconn } = require('../db/config')
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-// module.exports = router;
-
 router.get('/', function(req, res) {
-  // we first check if the 'contacts' table exists
-  // pgconn.query("SELECT EXISTS (SELECT FROM pg_tables WHERE tablename = 'plants')", function(err,results) {
+  // Check if 'plants' table exists
     pgconn.query("SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = 'plants')", function(err,results) {
     if (err) {
       console.log(err);
       res.render('index', { error: 'Database connection failure! '+err.stack, plants: null, title: 'Plant List' });
     }
 
-    // 'contacts' table does not exist. Show an empty table.
+    // If 'plants' table does not exist, display empty table.
     else if(results.rows[0].exists == false) {
       res.render('index', { error: null, plants: null, title: 'Plant List' });
     }
 
-    // 'contacts' table exists. Show the records.
+    // If 'plants' table exists, display plant records.
     else {
       pgconn.query('SELECT * FROM plants', function(err,results) {
         if (err) {
