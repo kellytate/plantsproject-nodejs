08ff0aec85b3f06c405b1bb1d888214568be7b10
kellytate <kelly.tate.dev@gmail.com>
2022-07-08 23:11:22 -0400

    updated

--- db/config.js
+++ db/config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://postgres:postgres@localhost:5432/plantsdb';
+const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://postgres:postgres@localhost:5432/plants';
 const { Pool } = require('pg');
 
 const pgconn = new Pool({
