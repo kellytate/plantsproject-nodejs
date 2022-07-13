0c4d943572aa6e84db61dc06526ca2d2a62d3ab2
kellytate <kelly.tate.dev@gmail.com>
2022-07-13 14:08:42 -0400

    for heroku db

--- db/config.js
+++ db/config.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
-const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://postgres:postgres@localhost:5432/plantsdb';
+// const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://postgres:postgres@localhost:5432/plantsdb';
+const DB_CONFIG = process.env.DATABASE_URL;
+
 const { Pool } = require('pg');
 
 const pgconn = new Pool({
