24a35aca8a2a92adbdaef3f20a197ce7235efe4b
kellytate <kelly.tate.dev@gmail.com>
2022-07-17 07:51:12 -0400

    corrected syntax

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,38 @@ const express = require('express');
 const router = express.Router();
 const { pgconn } = require('../db/config');
 
+// function queryBuilder(req) {
+//   const commonname = (req.body.commonname).trim();
+//   const scientificname = (req.body.scientificname).trim();
+//   const nickname = (req.body.nickname).trim();
+//   const datelastwatered = (req.body.datelastwatered);
+//   const datetowater = (req.body.datetowater);
+//   const id = (req.body.id);
+  
+//   let index = 0;
+
+//   let items = []
+//   let values = []
+//   let text = ''
+
+//   items.append(commonname, scientificname, nickname, datelastwatered, datetowater, id);
+
+//   for (item in items) {
+//     if (item.length > 0)
+//       values.append(item);
+//   }
+  
+
+//   text = "INSERT INTO plants ("
+//   str = "INSERT INTO plants (nickname, commonname, scientificname, dateLastWatered, dateToWater) VALUES($1::text, $2::text, $3::text, $4::date, $5::date)"
+
+
+//   return 
+
+
+// }
+
+
 
 router.get('/', function(req, res) {
   // Check if 'plants' table exists
@@ -45,7 +77,7 @@ router.post('/update', function(req,res) {
   const datetowater = (req.body.datetowater);
 
   let text = "SELECT EXISTS (SELECT FROM plants WHERE nickname = $1::text)"
-  let values = [nickname]
+  let values = [nickname.toLowerCase()];
   pgconn.query(text, values, function(err,results) {
     if (err) {
       console.log(err);
@@ -54,13 +86,13 @@ router.post('/update', function(req,res) {
 
     // If nickname does not exist in plants, create new plant.
     else if(results.rows[0].exists == false) {
-      text = "INSERT INTO plants (nickname, commonname, scientificname, dateLastWatered, dateToWater) VALUES($1::text, $2::text, $3::text, $4::date, $5::date)"
       values = [nickname, commonname, scientificname, datelastwatered, datetowater]
+      text = "INSERT INTO plants (nickname, commonname, scientificname, dateLastWatered, dateToWater) VALUES($1, $2, $3, $4, $5)"
     }
 
     else {
-      text = "UPDATE plants SET commonname = $1::text, scientificname = $2::text, datelastwatered = $3::date, datetowater = $4::date WHERE nickname = $5::text"
       values = [commonname, scientificname, datelastwatered, datetowater, nickname]
+      text = "UPDATE plants SET commonname = $1::text, scientificname = $2::text, datelastwatered = $3::date, datetowater = $4::date WHERE nickname = $5::text"
     }
 
       pgconn.query(text, values, function(err,results) {
@@ -76,4 +108,7 @@ router.post('/update', function(req,res) {
     });
   });
   
+
+
+
 module.exports = router;
