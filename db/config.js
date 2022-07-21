350f14e176881f06c15f5b27db7bdf953b696806
kellytate <kelly.tate.dev@gmail.com>
2022-07-21 15:34:24 -0400

    db conf

--- db/config.js
+++ db/config.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
-const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://postgres:postgres@localhost:5432/plantsdb';
+const DB_CONFIG = process.env.DATABASE_URL;
+
 const { Pool } = require('pg');
 
 const pgconn = new Pool({
     connectionString: DB_CONFIG,
-    ssl: false,
+    ssl: {
+      rejectUnauthorized: false
+    }
 });
   
 module.exports = { pgconn }
+
